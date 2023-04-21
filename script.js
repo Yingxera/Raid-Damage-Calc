6896eec2f131e82397339a1911738c8dd24fdd5b
Yingxera <frejashp4@gmail.com>
2023-04-21 16:22:55 +0200

    added debuff formula

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const euler = 2.71828;
-const division = 1500;
+const defDivision = 1500;
+const diffThreshold = -30;
 
 function CalcEHP() {
     let health = document.getElementById("inputHealth").value;
     let defence = document.getElementById("inputDefence").value;
 
-    let reduction = (0.85 * (1 - Math.pow(euler, -defence / division))).toFixed(4);
+    let reduction = (0.85 * (1 - Math.pow(euler, -defence / defDivision))).toFixed(4);
     let ehp = (health / (1 - reduction)).toFixed(0);
 
     document.getElementById("textMitigation").innerText = reduction * 100;
     document.getElementById("textEHP").innerText = ehp;
+}
+
+
+function CalcACC() {
+    let acc = document.getElementById("inputACC").value;
+    let res = document.getElementById("inputRES").value;
+
+    let chance;
+    if ((acc - res) <= diffThreshold) {
+        chance = 0.3 + 0.67 * (1 - Math.pow(euler, (3 * (0.3 - (res - acc) / 100))));
+    }
+    else {
+        chance = 0.033 + 0.27 * Math.pow(euler, (6 * ((res - acc) / 100) - 0.3));
+    }
+
+    document.getElementById("textDiff").innerText = acc - res;
+    document.getElementById("textACC").innerText = ((1 - chance) * 100).toFixed(4);
 }
